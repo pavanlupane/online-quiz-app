9c5dd7cf50b1317989473ca861038b5c3314e186
pavanlupane <pavanlupane@gmail.com>
2016-02-29 02:40:02 -0800

    quiz page designed V 1.1

--- testMaxScript.js
+++ testMaxScript.js
@@ -1,6 +1,9 @@
 var questionnireJSON;
 var totalQs = 0;
 var questionCnt=15;
+var currentQuestion;
+var answersKey = [];
+var answersSelected = [];
 
 $(document).ready(function(){
     /*Jquery code for CouchDB connection and data fetch*/ 
@@ -11,8 +14,16 @@ $(document).ready(function(){
         
          if (typeof totalQs != 'undefined'){
             console.log("Connected to DB successfully!");
-            console.log("Total number of questions in DB ::" + totalQs);    
-            }    
+            console.log("Total number of questions in DB ::" + totalQs);
+             
+            for(i=0;i<questionCnt;i++){
+                answersKey[i] = questionnireJSON.rows[0].value.qsbank[i].answer;
+            }
+//            console.log("Answers::");
+//            for (key in answersKey){
+//                console.log(answersKey[key]);
+//            }
+        }    
     });
     
     /*Load review table content Init*/
@@ -25,14 +36,56 @@ $(document).ready(function(){
     /*question onClick transition code*/
     $(".qLinks").click(function(event){ //question links
         
-        console.log(event.target.id);
+        //console.log(event.target.id);
         var qsID = event.target.id;
         var qsIndex = qsID.split("#").pop();    //ready to use question index
+        var questionString = null;
+        var ansOptionsString = null;
+        var saveAnsBtnString = null;
         console.log(qsIndex);
-
-        $("#status" + qsIndex).html('<span>Visited<span>').css("color","green"); //updated status with visited
+        currentQuestion = qsIndex;
         
+        if(answersSelected[qsIndex-1] == 0 || typeof answersSelected[qsIndex-1] == 'undefined'){
+            $("#status" + qsIndex).html('<span>Visited<span>').css("color","#800000"); //updated status with visited
+        }
+        
+        if(typeof answersSelected[qsIndex-1] == 'undefined'){
+            $('#prevAns').html('<span style="font-weight:bold; color:white;">Previous selected answer :: Not Answered</span>');
+        }else{
+            $('#prevAns').html('<span style="font-weight:bold; color:white;">Previous selected answer :: '+answersSelected[qsIndex-1]+'</span>');
+        }
+        
+        
+        $("#qOffset").html("<span>Q</span><span style=\"font-size: 25px; padding-left:5px;\">"+qsIndex+"</span>");   //loaded question header
+        
+        questionString = questionnireJSON.rows[0].value.qsbank[qsIndex-1].question;
+        //console.log(questionString);
+        $("#qData").html('<span>'+questionString+'</span>');    //loaded question string
+        
+        //console.log(questionnireJSON.rows[0].value.qsbank[qsIndex-1].ansOptions[0]);
+        ansOptionsString = "<input id=\"radio1\" type=\"radio\" name=\"ans\" value=\"1\">&nbsp&nbsp&nbsp"+questionnireJSON.rows[0].value.qsbank[qsIndex-1].ansOptions[0]+"<br>";
+        ansOptionsString += "<input id=\"radio2\" type=\"radio\" name=\"ans\" value=\"2\">&nbsp&nbsp&nbsp"+questionnireJSON.rows[0].value.qsbank[qsIndex-1].ansOptions[1]+"<br>";
+        ansOptionsString += "<input id=\"radio3\" type=\"radio\" name=\"ans\" value=\"3\">&nbsp&nbsp&nbsp"+questionnireJSON.rows[0].value.qsbank[qsIndex-1].ansOptions[2]+"<br>";
+        ansOptionsString += "<input id=\"radio4\" type=\"radio\" name=\"ans\" value=\"4\">&nbsp&nbsp&nbsp"+questionnireJSON.rows[0].value.qsbank[qsIndex-1].ansOptions[3]+"<br>";
+        
+        //console.log(ansOptionsString);
+        $("#ansOpsForm").html(ansOptionsString);        //loaded answer options
+        
+        $("#saveAnsBtn").css("visibility","visible");       //set save answer button on
     });
     
+    $("#saveAnsBtn").click(function(){
+        //$("input:radio[name='ans']:checked").val();
+        console.log("Current question is :: "+currentQuestion);
+        var selectedAns = $("input:radio[name='ans']:checked").val();
+        console.log("Selected ::"+selectedAns);
+        if (typeof selectedAns != 'undefined'){
+            answersSelected[currentQuestion-1]=selectedAns;     //saved selected answer in array
+            $("#status" + currentQuestion).html('<span>Answered<span>').css("color","green"); //updated status with visited
 
+            console.log(answersSelected); 
+            $('#prevAns').html('<span style="font-weight:bold; color:white;">Previous selected answer :: '+selectedAns+'</span>');
+        }
+       
+    });
 });
